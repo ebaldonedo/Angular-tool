6f31e35ccedcf3e94bd5c73fb42820d08d59cebf
Enmanuel <50787415+ebaldonedo@users.noreply.github.com>
2024-04-29 22:36:03 -0400

    update
    update data

--- AngularTool/src/app/Rick_Morty/rick-morty-chars/rick-morty-chars.component.ts
+++ AngularTool/src/app/Rick_Morty/rick-morty-chars/rick-morty-chars.component.ts
@@ -2,35 +2,27 @@ import { Component } from '@angular/core';
 import { RickMortyApiService } from '../../services/rick-morty-api.service.service';
 import { RickMortyChar } from '../../models/Rick-Morty-char.model';
 
-
-
 @Component({
   selector: 'app-rick-morty-chars',
   templateUrl: './rick-morty-chars.component.html',
-  styleUrls: ['./rick-morty-chars.component.css']
+  styleUrls: ['./rick-morty-chars.component.css'],
 })
 export class RickMortyCharsComponent {
+  constructor(private rickMortyApi: RickMortyApiService) {}
 
-  constructor(private rickMortyApi: RickMortyApiService) { }
-
-
-characters: RickMortyChar[] = [];
-
-
+  characters: RickMortyChar[] = [];
 
   ngOnInit(): void {
     this.getChars();
-
+  }
+  selectChar(char: RickMortyChar): void {
+    console.log(char);
   }
 
   getChars(): void {
-                                                      //implicit destructuring
-    this.rickMortyApi.getRickMortyChars().subscribe( ( { results } ) => {
-        this.characters = results;
-    }
-    );
-
-
+    //implicit destructuring
+    this.rickMortyApi.getRickMortyChars().subscribe(({ results }) => {
+      this.characters = results;
+    });
   }
-
 }
--- AngularTool/src/app/components/pokeapi/pokeapi.component.ts
+++ AngularTool/src/app/components/pokeapi/pokeapi.component.ts
@@ -1,79 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import {PokemonApiService  } from '../../services/pokemon-api.service';
+import { PokemonApiService } from '../../services/pokemon-api.service';
 import { Pokemon } from '../../models/pokemon.model';
-
 
 @Component({
   selector: 'PokeapiComponent',
   templateUrl: './pokeapi.component.html',
   styleUrls: ['./pokeapi.component.css'],
-
-
 })
 export class PokeapiComponent implements OnInit {
-//1.-Declaracion de variables
+  //1.-Declaracion de variables
   pokemons: Pokemon[] = [];
 
   pokemonsJhoto: Pokemon[] = [];
 
-
   pokemonsHoenn: Pokemon[] = [];
 
+  pokeimg: string =
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
+  selectPokemon(pokemon: Pokemon): void {
+    console.log(pokemon);
+  }
 
+  //2.-Consumimos el servicio a traves del constructor
+  constructor(private pokemonApi: PokemonApiService) {}
 
-
-  pokeimg: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-
-//2.-Consumimos el servicio a traves del constructor
-  constructor(private pokemonApi: PokemonApiService) { }
-
-//3.-Llamado de la funcion para consumir api
+  //3.-Llamado de la funcion para consumir api
   ngOnInit(): void {
     this.getPokemons();
     this.getJhoto();
     this.getHoenn();
   }
 
-  getpokeimgs(i:any):any{
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
-
+  getpokeimgs(i: number): string {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
   }
 
-//funcion para consumir servicio de la api
+  //funcion para consumir servicio de la api
   getPokemons(): void {
-                                            //desestructuracion de objetos
-    this.pokemonApi.getPokemons().subscribe(({results})=> {
-        this.pokemons = results;
-      });
+    //desestructuracion de objetos
+    this.pokemonApi.getPokemons().subscribe(({ results }) => {
+      this.pokemons = results;
+    });
   }
-//funcion para llamar al servicio Jhoto
+  //funcion para llamar al servicio Jhoto
   getJhoto(): void {
-    this.pokemonApi.getJhoto().subscribe(({results}) => {
-        this.pokemonsJhoto = results;
-      });
+    this.pokemonApi.getJhoto().subscribe(({ results }) => {
+      this.pokemonsJhoto = results;
+    });
   }
 
   //funcion para llamar a al servicio de hoenn
   getHoenn(): void {
-    this.pokemonApi.getHoenn().subscribe(({results}) => {
-        this.pokemonsHoenn = results;
-      });
+    this.pokemonApi.getHoenn().subscribe(({ results }) => {
+      this.pokemonsHoenn = results;
+    });
   }
 
   //Funcion para mostrar la primera letra de un string en mayuscula
 
-   capitalizeFirstLetter(str: string): string {
+  capitalizeFirstLetter(str: string): string {
     const firstLetter = str.charAt(0);
     const capitalizedFirstLetter = firstLetter.toUpperCase();
     const restOfStr = str.slice(1);
     const capitalizedStr = capitalizedFirstLetter + restOfStr;
     return capitalizedStr;
   }
-
-
-
-
 }
-
-
